1c5e9e700c46b99db750da87c3ff760aaf03da5e
Quentin M <quentin.mulliez@edu.ece.fr>
2019-10-26 11:34:33 +0200

    code clean 1

--- js/script.js
+++ js/script.js
@@ -36,45 +36,22 @@ function start() {
     apiWeather
     .fetchDaysForecast()
     .then(function(response) {
-      // Récupère la donnée d'une API
       const data = response.data;
 
-      console.log(response.data);
-
-      main = [];
-      description = [];
-      temp = [];
-      icon =[];
-
-
-      for(let i = 1; i < 4; i++){
-        main.push(data.list[i].weather[0].main);
-        description.push(data.list[i].weather[0].description);
-        temp.push(data.list[i].temp.day);
-        icon.push(apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon));
-      }
-      
-      // document.getElementById('today-forecast-main').innerHTML = main;
-      // document.getElementById('today-forecast-more-info').innerHTML = description;
-      // document.getElementById('icon-weather-container').innerHTML = icon;
-      // document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-
-
+      //recuperation des balises HTML
       forMain = document.getElementsByClassName('mainHTML');
       forDescription = document.getElementsByClassName('descriptionHTML');
       forIcon = document.getElementsByClassName('iconHTML');
       forTemp = document.getElementsByClassName('tempHTML');
 
+      //Et on y met les données, c est pas sorcier non ?
       for(let i = 0; i < 3; i++){
-        forMain[i].innerHTML = main[i];
-        forDescription[i].innerHTML = description[i];
-        forIcon[i].innerHTML = icon[i];
-        myTemp = temp[i];
+        forMain[i].innerHTML = data.list[i + 1].weather[0].main;
+        forDescription[i].innerHTML = data.list[i + 1].weather[0].description;
+        forIcon[i].innerHTML = apiWeather.getHTMLElementFromIcon(data.list[i + 1].weather[0].icon);
+        myTemp = data.list[i + 1].temp.day;
         forTemp[i].innerHTML = `${myTemp}°C`;
       }
-
-
-      //console.log(main);
 
     })
     .catch(function(error) {
